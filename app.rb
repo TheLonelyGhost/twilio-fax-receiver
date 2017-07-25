e9ca6923ff3e29b11c1f3470070ebfdcd5c252b2
David Alexander <davidpaulalexander@gmail.com>
2017-07-25 14:08:39 -0400

    Initial commit

---
+++ app.rb
@@ -0,0 +1,49 @@
+require 'sinatra'
+
+@authorized_numbers = ENV['AUTHORIZED_FAX_RECIPIENTS'].split(';').map {|num| num.strip.freeze }.freeze
+
+get '/' do
+  # TODO: no homepage here, see `/status`
+  redirect '/status'
+end
+
+get '/status' do
+  content_type 'text/plain'
+
+  status 200
+  body 'I am online!'
+end
+
+post '/fax/receive/start' do
+  reject_response = <<-EOF
+  <Response>
+    <Reject />
+  </Response>
+  EOF
+  allow_response = <<-EOF
+  <Response>
+    <Receive method="POST" action="/fax/receive/done" mediaType="application/pdf" storeMedia="true" />
+  </Response>
+  EOF
+  output = reject_response
+
+  content_type 'text/xml'
+  catch(:processed) do
+    throw :processed unless @authorized_numbers.include? params['To']
+
+    output = allow_response
+  end
+  body output
+end
+
+post '/fax/receive/done' do
+  if params['ErrorCode'].to_i > 0
+    logger.fatal(format('TWILIO ERROR -> (%d) %s', params['ErrorCode'], params['ErrorMessage']))
+    # We're still returning 200 so Twilio knows we got the message that there was an error
+  else
+    logger.info(format('RECEIVING FAX -> (Pages: %d) %s', params['NumPages'], params['FaxSid'].inspect))
+  end
+
+  status 200
+  body ''
+end
